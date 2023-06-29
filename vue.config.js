b49c70333d9a46966f663fff854ea08a186ab8ff
giserliyang <liyang_qifengle@126.com>
2023-06-29 17:56:29 +0800

    feat:配置全局sass/scss,并在项目中改变scss变量以修改elementUi样式

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  // *配置全局sass/scss，详见：https://blog.csdn.net/qq_36604536/article/details/124256300
+  css: {
+    extract: true,
+    sourceMap: false,
+    loaderOptions: {
+      sass: {
+        additionalData: `@import "./src/styles/_variables.scss";`,
+      },
+    },
+  },
 })
