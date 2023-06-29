082e4f22c7d09e637a31121ab83de236d16b6bd9
giserliyang <liyang_qifengle@126.com>
2023-06-29 16:09:18 +0800

    feat:引用elementui

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import './styles/index.scss'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI, { size: 'small' })
 
 new Vue({
   router,
