ef1bb7b23a2ae84d7d70e3ecde09423c59ffc8b3
giserliyang <liyang_qifengle@126.com>
2023-07-04 20:42:21 +0800

    feat:添加reset(重构)样式表

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,5 +2,32 @@
   <div class="about">
     <h1>This is an about page</h1>
     <el-button>默认按钮</el-button>
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      tableData:[]
+    }
+  },
+}
+</script>
